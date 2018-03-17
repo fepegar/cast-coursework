2ae7b51437b3ef857376008debb1308ed5484ab2
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-17 01:52:31 +0000

    Add morphological operations

--- cast/image.py
+++ cast/image.py
@@ -1,6 +1,8 @@
+from skimage import img_as_uint
 from skimage.filters import frangi
 from skimage.color import rgb2grey
 from skimage.io import imread, imsave
+from skimage.morphology import binary_erosion, binary_closing
 
 from .path import ensure_dir
 
@@ -11,6 +13,8 @@ def read(path):
 
 def write(image, path):
     ensure_dir(path)
+    if image.dtype == bool:
+        image = img_as_uint(image)
     return imsave(str(path), image)
 
 
@@ -18,3 +22,14 @@ def frangi_filter(image):
     if image.ndim == 3:
         image = rgb2grey(image)
     return frangi(image)
+
+
+def erode(image, times=1):
+    eroded = image
+    for _ in range(times):
+        eroded = binary_erosion(eroded)
+    return eroded
+
+
+def closing(image):
+    return binary_closing(image)
