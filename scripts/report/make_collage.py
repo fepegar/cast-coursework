0efc75ac317da3c87d2268fcbc532034eaa855f6
Fernando <fepegar@gmail.com>
2018-03-21 19:24:17 +0000

    Add comparison between humans to collage

--- scripts/report/make_collage.py
+++ scripts/report/make_collage.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from skimage import io
 import matplotlib.pyplot as plt
 
 from cast import test_set
@@ -19,21 +18,32 @@ worst = test_set.samples[2]
 samples = best, median, worst
 
 
-fig, axes = plt.subplots(len(samples), 5, sharex=True, sharey=True)
+def normalise(image):
+    image = image.astype(float)
+    image -= image.min()
+    image /= image.max()
+    return image
+
+
+fig, axes = plt.subplots(len(samples), 6, sharex=True, sharey=True)
 
 for axis in fig.axes:
     axis.set_axis_off()
 
 for row, sample in enumerate(samples):
+    labels1 = sample.labels
     axes[row, 0].imshow(sample.rgb_data)
-    prediction = io.imread(sample.prediction_prob_path).astype(float)
-    prediction /= prediction.max()
+    prediction = normalise(im.read(sample.prediction_prob_path))
     axes[row, 1].imshow(prediction)
-    driu = io.imread(sample.driu_path).astype(float)
-    driu /= driu.max()
-    axes[row, 2].imshow(im.compare(sample.labels, prediction > 0.5))
+    driu = normalise(im.read(sample.driu_path).astype(float))
+    axes[row, 2].imshow(im.compare(labels1, prediction > 0.5))
     axes[row, 3].imshow(driu)
-    axes[row, 4].imshow(im.compare(sample.labels, driu > 0.5))
+    axes[row, 4].imshow(im.compare(labels1, driu > 0.5))
+    labels2 = normalise(im.read(sample.manual_2_path)[..., 0])
+    import numpy as np
+    print(np.unique(labels1))
+    print(np.unique(labels2))
+    axes[row, 5].imshow(im.compare(labels1, labels2))
 
 # This does not seem to work
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1,
