7163039b33ef436321fe80bc2fc02858a59105b7
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-21 12:52:16 +0000

    Add datasets objects to cast module

--- cast/__init__.py
+++ cast/__init__.py
@@ -1,1 +1,10 @@
+from pathlib import Path
+
 from .dataset import DataSet
+
+dataset_dir = Path(Path.home(), 'Desktop', 'DRIVE')
+training_dir = dataset_dir / 'training'
+test_dir = dataset_dir / 'test'
+
+training_set = DataSet(training_dir)
+test_set = DataSet(test_dir)
