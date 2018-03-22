5d0ecda2ed212f636c8e503e9e7fa758fe4d945b
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-22 00:04:10 +0000

    Make collage figure tighter

--- scripts/report/make_collage.py
+++ scripts/report/make_collage.py
@@ -6,18 +6,6 @@ from cast import test_set
 from cast import image as im
 
 
-repo_dir = Path(__file__).parents[2]
-figures_dir = repo_dir / 'latex' / 'figures'
-output_path = figures_dir / 'collage.png'
-
-# Previously measured
-best = test_set.samples[1]
-median = test_set.samples[12]
-worst = test_set.samples[2]
-
-samples = best, median, worst
-
-
 def normalise(image):
     image = image.astype(float)
     image -= image.min()
@@ -25,25 +13,46 @@ def normalise(image):
     return image
 
 
-fig, axes = plt.subplots(len(samples), 6, sharex=True, sharey=True)
+def main():
+    repo_dir = Path(__file__).parents[2]
+    figures_dir = repo_dir / 'latex' / 'figures'
+    output_path = figures_dir / 'collage.png'
 
-for axis in fig.axes:
-    axis.set_axis_off()
+    # Previously measured
+    best = test_set.samples[1]
+    median = test_set.samples[12]
+    worst = test_set.samples[2]
 
-for row, sample in enumerate(samples):
-    labels1 = sample.labels
-    axes[row, 0].imshow(sample.rgb_data)
-    prediction = normalise(im.read(sample.prediction_prob_path))
-    axes[row, 1].imshow(im.grey2rgb(prediction, mask=sample.mask))
-    driu = normalise(im.read(sample.driu_path).astype(float))
-    axes[row, 2].imshow(im.compare(labels1, prediction > 0.5))
-    axes[row, 3].imshow(im.grey2rgb(driu, mask=sample.mask))
-    axes[row, 4].imshow(im.compare(labels1, driu > 0.5))
-    labels2 = normalise(im.read(sample.manual_2_path)[..., 0])
-    axes[row, 5].imshow(im.compare(labels1, labels2))
+    samples = best, median, worst
 
-# This does not seem to work
-plt.subplots_adjust(left=0, bottom=0, right=1, top=1,
-                    wspace=0.05, hspace=0.05)
+    fig, axes = plt.subplots(3, 6, figsize=(6, 3))
 
-fig.savefig(output_path, dpi=400, bbox_inches='tight')
+    for row, sample in enumerate(samples):
+        labels1 = sample.labels
+        prediction = normalise(im.read(sample.prediction_prob_path))
+        driu = normalise(im.read(sample.driu_path).astype(float))
+        labels2 = normalise(im.read(sample.manual_2_path)[..., 0])
+
+        images = []
+        images.append(sample.rgb_data)
+        images.append(im.grey2rgb(prediction, mask=sample.mask))
+        images.append(im.compare(labels1, prediction > 0.5))
+        images.append(im.grey2rgb(driu, mask=sample.mask))
+        images.append(im.compare(labels1, driu > 0.5))
+        images.append(im.compare(labels1, labels2))
+
+        for col, image in enumerate(images):
+            ax = axes[row, col]
+            ax.set_axis_off()
+            ax.imshow(image)
+
+    fig.subplots_adjust(left=0, bottom=0,
+                        right=1, top=1,
+                        wspace=0.05,
+                        hspace=0.05)
+
+    fig.savefig(output_path, dpi=400)
+
+
+if __name__ == '__main__':
+    main()
